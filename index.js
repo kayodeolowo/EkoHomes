3cb353d3e496d25d06139bbb69007f129589c1d1
Kayode <officialkayodeolowo@gmail.com>
2024-12-18 09:09:11 +0100

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const cors = require("cors"); // Import cors
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const connectDatabase = require('./config/database');
 
 // Connect to the database
@@ -23,7 +23,7 @@ app.use("/ekohomes/api/v1/", require("./routes/propertyRoutes"));
 app.use(errorHandler);
 
 // Start server
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT,  () => {
     console.log(`Server running on port ${PORT}`);
 });
 
